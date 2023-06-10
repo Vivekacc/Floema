bb829dcfe0337f3711d3fd1850996aea56e57fb4
Vivekacc <118170207+Vivekacc@users.noreply.github.com>
2023-06-10 10:56:57 +0530

    another_
    another_

---
+++ app/index.js
@@ -0,0 +1,83 @@
+import each from 'lodash/each';
+
+import About from 'pages/About'
+import Collections from 'pages/Collections/'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
+
+class App{
+    constructor () {
+        this.createContent()
+        this.createPages()
+        
+        this.addLinkListeners()
+    }
+    
+    createContent(){
+        
+        this.content = document.querySelector('.content');
+        this.template = this.content.getAttribute('data-template');
+        
+    }
+    
+    createPages(){
+        this.pages = {
+            about : new About(),
+            collections : new Collections(),
+            home : new Home(),
+            detail : new Detail(),
+        };
+        
+        this.page = this.pages[this.template];
+        this.page.create();
+        this.page.show();
+    }
+    
+    async onChange (url) { 
+        // await this.page.hide()
+        const request = await window.fetch(url)
+        
+        if (request.status === 200){
+            const html = await request.text()
+            
+            const div = document.createElement('div')
+            
+            div.innerHTML = html
+            
+            const divContent = div.querySelector('.content')
+            
+            // this.template = divContent.getAttribute('data-template')
+            
+            // this.content.setAttribute('data-template', this.template)
+            this.content.innerHTML = divContent.innerHTML
+            
+            // this.page = this.pages[this.template]
+            // this.page.create()
+
+            // this.page.show()
+
+            console.log(this.content)
+              
+        }  else{
+                console.log('error');
+            }
+            
+        }
+        
+        addLinkListeners() {
+            const links = document.querySelectorAll('a');
+        
+            each(links, link => {
+              link.onclick = event => {
+                event.preventDefault();
+        
+                const { href } = link;
+                this.onChange(href);
+                
+              };
+            });
+        }
+}
+
+
+new App();
